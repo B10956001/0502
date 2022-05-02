97ee5237b4d4af600310c71d0045c39acec95b64
user <user@DESKTOP-6JBEF5K>
2022-05-02 10:15:28 +0800

    0.8

--- TCP_Client/Form1.Designer.cs
+++ TCP_Client/Form1.Designer.cs
@@ -36,11 +36,17 @@ namespace TCP_Client
             this.label3 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_Send = new System.Windows.Forms.TextBox();
+            this.button_send = new System.Windows.Forms.Button();
+            this.button_snndAll = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(201, 55);
+            this.textBox1.Location = new System.Drawing.Point(131, 6);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 0;
@@ -49,7 +55,7 @@ namespace TCP_Client
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(100, 58);
+            this.label1.Location = new System.Drawing.Point(30, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(82, 19);
             this.label1.TabIndex = 1;
@@ -59,7 +65,7 @@ namespace TCP_Client
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label2.Location = new System.Drawing.Point(363, 58);
+            this.label2.Location = new System.Drawing.Point(293, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(96, 19);
             this.label2.TabIndex = 3;
@@ -67,7 +73,7 @@ namespace TCP_Client
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(475, 55);
+            this.textBox2.Location = new System.Drawing.Point(405, 6);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 22);
             this.textBox2.TabIndex = 2;
@@ -76,7 +82,7 @@ namespace TCP_Client
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label3.Location = new System.Drawing.Point(100, 126);
+            this.label3.Location = new System.Drawing.Point(30, 49);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(85, 19);
             this.label3.TabIndex = 5;
@@ -84,27 +90,87 @@ namespace TCP_Client
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(201, 123);
+            this.textBox3.Location = new System.Drawing.Point(131, 46);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 22);
             this.textBox3.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button1.Location = new System.Drawing.Point(367, 120);
+            this.button1.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(416, 40);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(126, 30);
+            this.button1.Size = new System.Drawing.Size(80, 30);
             this.button1.TabIndex = 7;
             this.button1.Text = "登入伺服器";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(34, 93);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(471, 268);
+            this.textBox4.TabIndex = 8;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(526, 93);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 268);
+            this.listBox1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(27, 392);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 19);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "發言內容";
+            // 
+            // textBox_Send
+            // 
+            this.textBox_Send.Location = new System.Drawing.Point(121, 392);
+            this.textBox_Send.Name = "textBox_Send";
+            this.textBox_Send.Size = new System.Drawing.Size(384, 22);
+            this.textBox_Send.TabIndex = 11;
+            // 
+            // button_send
+            // 
+            this.button_send.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button_send.Location = new System.Drawing.Point(545, 386);
+            this.button_send.Name = "button_send";
+            this.button_send.Size = new System.Drawing.Size(77, 30);
+            this.button_send.TabIndex = 12;
+            this.button_send.Text = "送出";
+            this.button_send.UseVisualStyleBackColor = true;
+            // 
+            // button_snndAll
+            // 
+            this.button_snndAll.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button_snndAll.Location = new System.Drawing.Point(562, 6);
+            this.button_snndAll.Name = "button_snndAll";
+            this.button_snndAll.Size = new System.Drawing.Size(84, 22);
+            this.button_snndAll.TabIndex = 13;
+            this.button_snndAll.Text = "廣播";
+            this.button_snndAll.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_snndAll);
+            this.Controls.Add(this.button_send);
+            this.Controls.Add(this.textBox_Send);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox3);
@@ -129,6 +195,12 @@ namespace TCP_Client
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_Send;
+        private System.Windows.Forms.Button button_send;
+        private System.Windows.Forms.Button button_snndAll;
     }
 }
 
